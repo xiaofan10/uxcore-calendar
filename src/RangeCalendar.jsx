dfd0bace389929bbe519a5b0001874ecc03de19c
wb-fjp324710 <wb-fjp324710@alibaba-inc.com>
2017-10-18 17:33:15 +0800

    传入样式不做处理

--- src/RangeCalendar.jsx
+++ src/RangeCalendar.jsx
@@ -176,7 +176,7 @@ class Calendar extends React.Component {
 
     const triggerStyle = {};
     if (p.inputWidth) {
-      triggerStyle.width = `${p.inputWidth}px`;
+      triggerStyle.width = p.inputWidth;
     }
 
     const inputClassName = classnames('kuma-input', {
